a6a4cf9ae52bb5b86a544b1ae0d7e0d737f0a2c5
chaeshee0908 <chaeshee0908@naver.com>
2023-01-30 17:29:17 +0900

    [feat] userDao 생성

---
+++ src/main/java/com/toby/springstudy/user/dao/SimpleConnectionMaker.java
@@ -0,0 +1,15 @@
+package com.toby.springstudy.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {
+    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/toby", "seunghee", "cotmdgml019@"
+        );
+        return c;
+    }
+}
---
+++ src/main/java/com/toby/springstudy/user/dao/UserDao.java
@@ -0,0 +1,51 @@
+package com.toby.springstudy.user.dao;
+
+import com.toby.springstudy.user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+
+    private SimpleConnectionMaker simpleConnectionMaker;
+
+    public UserDao() {
+        simpleConnectionMaker = new SimpleConnectionMaker();
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection c = simpleConnectionMaker.makeNewConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values(?,?,?)"
+        );
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close(); // statement 종료
+        c.close();  // connection 종료
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection c = simpleConnectionMaker.makeNewConnection();
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+}
