7d1a5e6cb788cc2714000cf2431261cb77114b78
chaeshee0908 <chaeshee0908@naver.com>
2023-02-04 16:22:03 +0900

    [feat] 관계설정 책임의 분리

--- src/main/java/com/toby/springstudy/user/dao/UserDaoTest.java
+++ src/main/java/com/toby/springstudy/user/dao/UserDaoTest.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
+        ConnectionMaker connectionMaker = new DConnectionMaker();   // 구현 클래스 결정
 
         UserDao dao = new UserDao(connectionMaker); // 두 오브젝트 사이의 의존관계 설정 효과
     }
